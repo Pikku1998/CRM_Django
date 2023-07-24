3c6501598c00ec36d5018fbcbb5536998e8dedc6
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-24 17:58:07 +0530

    manual registration form

--- crm_website/urls.py
+++ crm_website/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index, logout_view
+from .views import index,register_view, logout_view
 
 urlpatterns = [
     path('', index, name='home'),  
-    path('logout', logout_view, name='logout'), 
+    path('logout/', logout_view, name='logout'), 
+    path('register/', register_view, name='register')
 ]
--- crm_website/views.py
+++ crm_website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 def index(request):
@@ -16,6 +17,30 @@ def index(request):
             return render(request, 'home.html')
 
     return render(request, 'home.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request,'username already exists..')
+            return redirect('register')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'email already registered..')
+            return redirect('register')
+        if password1 != password2:
+            messages.error(request, 'passwords didn\'t match..')
+            return redirect('register')
+        user = User.objects.create_user(username=username,email=email, password=password1)
+        messages.success(request, "Registered Successfully, Login with your credentials...")
+        return redirect('home')
+    return render(request, 'register.html')
+
+        
+
 
 def logout_view(request):
     logout(request)
