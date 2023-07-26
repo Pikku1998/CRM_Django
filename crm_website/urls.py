258e6a5eec7b297b7cb90fac701168e00bfa9036
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-26 19:10:15 +0530

    view records on homepage after logging

--- crm_website/urls.py
+++ crm_website/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import index, signup_view, register_view, logout_view
+from .views import index, signup_view, register_view, logout_view, view_record, add_record
 
 urlpatterns = [
     path('', index, name='home'),  
     path('logout/', logout_view, name='logout'), 
-    path('signup/', signup_view, name='signup')
+    path('signup/', signup_view, name='signup'),
+    path('record/<int:pk>', view_record, name='view_record'),
+    path('add_record/', add_record, name='add_record'),
+
 ]
--- crm_website/views.py
+++ crm_website/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, AddRecordForm
+from .models import Record
 
 def index(request):
     if request.method == 'POST':
@@ -16,7 +17,9 @@ def index(request):
         else:
             messages.error(request, "Please check your credentials..")
             return render(request, 'home.html')
-
+    if request.user.is_authenticated:
+        records = Record.objects.all()
+        return render(request, 'home.html', {'records': records})
     return render(request, 'home.html')
 
 # register view using UserCreationForm
@@ -68,5 +71,21 @@ def logout_view(request):
     logout(request)
     messages.success(request, "You have been logged out..")
     return redirect('home')
+
+
+def view_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(pk=pk)
+        return render(request, 'record_detail.html', {'record':record})
+    return redirect('home')
+
+def add_record(request):
+    if request.method == "POST":
+        form = AddRecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = AddRecordForm()
+    return render(request, 'add_record.html', {'form': form})
+
     
 
